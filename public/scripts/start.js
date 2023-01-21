9965ed04d429e13aa9bae4dcff13d099fd79afc2
ranatasha <rubezhovana@mail.ru>
2023-01-22 01:59:02 +0300

    minor changes to prepare loading and updating leaderboard

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let tmp = localStorage['game.leaderBoard'];
-    const tableBox = document.getElementById('leaderboard-results');
-    if (!tmp) {
-        let p = document.createElement('p');
-        p.innerHTML = 'There are no players results yet';
-        p.classList.add('leaderboard-warning')
-        tableBox.appendChild(p);
-    }
+    loadLeaderboard()
+
 })
 
 function savePlayer() {
@@ -14,6 +8,41 @@ function savePlayer() {
     const name = form.querySelector('[name="username"]');
     if (name.value) {
         localStorage["game.username"] = name.value;
-        console.log("Storing Player - YES");
     }
 }
+
+function loadLeaderboard() {
+    let tmp = localStorage['game.leaderBoard'];
+    const tableBox = document.getElementById('leaderboard-results');
+    if (!tmp) {     // if leaderboard is empty
+        let p = document.createElement('p');
+        p.innerHTML = 'There are no players results yet';
+        p.classList.add('leaderboard-warning')
+        tableBox.appendChild(p);
+    } else {
+        updateLeaderboard()
+    }
+    
+}
+
+function updateLeaderboard() {
+    let arr = JSON.parse(localStorage.getItem('game.leaderBoard'));
+    // check: Is there user in the leaderboard for updating his result?
+    let userWas = false;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][0] === localStorage['game.username']){
+            if(localStorage['game.score'] > arr[i][1])
+                arr[i][1] = localStorage['game.score'];
+            userWas = true;
+        }
+    }
+    if(!userWas)
+        arr.push([localStorage['game.username'], localStorage['game.score']]);
+    arr.sort((function(index){
+        return function(a, b){
+            return (a[index] === b[index] ? 0 : (a[index] < b[index] ? 1 : -1));
+        };
+    })(1));
+    localStorage.setItem('game.leaderBoard', JSON.stringify(arr));
+}
+
